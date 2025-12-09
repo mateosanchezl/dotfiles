b9e5be23efe755bd55ed8da9608c193085b1e4e4
Mateo Sanchez <mateosanchez.msl@gmail.com>
2025-12-09 16:14:39 +0000

    remove bufferline slop

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -4,7 +4,8 @@ return {
   version = "*",
   dependencies = "nvim-tree/nvim-web-devicons",
   config = function()
-    require("bufferline").setup {
+    local bufferline = require "bufferline"
+    bufferline.setup {
       options = {
         diagnostics = "nvim_lsp",
         offsets = {
@@ -15,7 +16,20 @@ return {
             separator = true,
           },
         },
+        buffer_close_icon = "",
+        close_icon = "",
         always_show_bufferline = false,
+        indicator = {
+          style = "underline",
+        },
+        show_close_icon = false,
+        separator_style = "thin",
+      },
+      highlights = {
+        buffer_selected = {
+          italic = false,
+          bold = false,
+        },
       },
     }
   end,
